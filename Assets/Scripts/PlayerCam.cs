a4a09f0cda1dbb2373204ed1c03fc490e0e02bb6
Jer-Tran <jeremy.tran83@gmail.com>
2024-08-26 18:58:58 +1000

    simple player camera movement

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public Vector3 _direction;
+    public Transform _player;
+    public float _dist;
+    public Transform _cameraOffset;
+
+    public float angleLock;
+    public float turnSpeed;
+    public float zoomSpeed;
+    public float maxZoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(_player.position, transform.position);
+
+        // Change distance
+        _dist += -1 * Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        _dist = Mathf.Clamp(_dist, 1, maxZoom);
+
+        float x = Input.GetAxis("Mouse X") * turnSpeed;
+        // If holding LMB, rotate character
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 rot = _player.rotation.eulerAngles;
+            rot.y += x;
+            _player.rotation = Quaternion.Euler(rot);
+        }
+        // If holding RMB, rotate camera
+        else if (Input.GetMouseButton(1))
+        {
+            Vector3 rot = _cameraOffset.rotation.eulerAngles;
+            float y = Input.GetAxis("Mouse Y") * turnSpeed;
+            rot.x += -1 * y; // Values flipped cause of Quaternion stuff
+            if (rot.x > 180) rot.x -= 360;
+            rot.x = Mathf.Clamp(rot.x, -1 * angleLock, angleLock);
+            rot.y += x;
+            _cameraOffset.rotation = Quaternion.Euler(rot);
+
+        }
+        Vector3 direction = -1 * _cameraOffset.forward;
+        transform.position = _player.position + direction * _dist;
+        transform.LookAt(_player);
+
+
+    }
+}
